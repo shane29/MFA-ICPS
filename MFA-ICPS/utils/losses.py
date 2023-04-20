abe626c3d7d56ed87e16a10da2362009525963ba
shane <1095406694@qq.com>
2023-04-20 10:22:17 +0800

    add large file

---
+++ MFA-ICPS/utils/losses.py
@@ -0,0 +1,463 @@
+import cv2 as cv
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+import numpy as np
+from scipy.ndimage.morphology import distance_transform_edt as edt
+from scipy.ndimage import convolve
+
+def att_mse_loss(mask, cams):
+    """Takes softmax on both sides and returns MSE loss
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert mask.size() == cams.size() and len(mask.size()) == 5
+    mse_loss = F.mse_loss(mask, cams, reduction='none').sum((2, 3, 4))
+    norm = (mask.sum((2, 3, 4)) + cams.sum((2, 3, 4))).sum()
+    mse_loss = torch.sum(mse_loss) / torch.clamp(norm, min=1e-5)
+    return mse_loss
+
+def dice_loss(score, target):
+    target = target.float()
+    smooth = 1e-5
+    intersect = torch.sum(score * target)
+    y_sum = torch.sum(target * target)
+    z_sum = torch.sum(score * score)
+    loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+    loss = 1 - loss
+    return loss
+
+def Binary_dice_loss(predictive, target, ep=1e-8):
+    intersection = 2 * torch.sum(predictive * target) + ep
+    union = torch.sum(predictive) + torch.sum(target) + ep
+    loss = 1 - intersection / union
+    return loss
+
+def kl_loss(inputs, targets, ep=1e-8):
+    kl_loss=nn.KLDivLoss(reduction='mean')
+    consist_loss = kl_loss(torch.log(inputs+ep), targets)
+    return consist_loss
+
+def soft_ce_loss(inputs, target, ep=1e-8):
+    logprobs = torch.log(inputs+ep)
+    return  torch.mean(-(target[:,0,...]*logprobs[:,0,...]+target[:,1,...]*logprobs[:,1,...]))
+
+def mse_loss(input1, input2):
+    return torch.mean((input1 - input2)**2)
+
+class DiceLoss(nn.Module):
+    def __init__(self, n_classes):
+        super(DiceLoss, self).__init__()
+        self.n_classes = n_classes
+
+    def _one_hot_encoder(self, input_tensor):
+        tensor_list = []
+        for i in range(self.n_classes):
+            temp_prob = input_tensor == i * torch.ones_like(input_tensor)
+            tensor_list.append(temp_prob)
+        output_tensor = torch.cat(tensor_list, dim=1)
+        return output_tensor.float()
+
+    def _dice_loss(self, score, target):
+        target = target.float()
+        smooth = 1e-10
+        intersection = torch.sum(score * target)
+        union = torch.sum(score * score) + torch.sum(target * target) + smooth
+        loss = 1 - intersection / union
+        return loss
+
+    def forward(self, inputs, target, weight=None, softmax=False):
+        if softmax:
+            inputs = torch.softmax(inputs, dim=1)
+        target = self._one_hot_encoder(target)
+        if weight is None:
+            weight = [1] * self.n_classes
+        assert inputs.size() == target.size(), 'predict & target shape do not match'
+        class_wise_dice = []
+        loss = 0.0
+        for i in range(0, self.n_classes):
+            dice = self._dice_loss(inputs[:, i], target[:, i])
+            class_wise_dice.append(1.0 - dice.item())
+            loss += dice * weight[i]
+        return loss / self.n_classes
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=5, alpha=0.5, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha, (float, int)):
+            self.alpha = torch.Tensor([alpha, 1-alpha])
+        if isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim() > 2:
+            # N,C,H,W => N,C,H*W
+            input = input.view(input.size(0), input.size(1), -1)
+            input = input.transpose(1, 2)    # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1, input.size(2))   # N,H*W,C => N*H*W,C
+        target = target.view(-1, 1)
+
+        logpt = F.log_softmax(input, dim=1)
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type() != input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
+
+class HausdorffDTLoss(nn.Module):
+    """Binary Hausdorff loss based on distance transform"""
+
+    def __init__(self, alpha=2.0, **kwargs):
+        super(HausdorffDTLoss, self).__init__()
+        self.alpha = alpha
+
+    @torch.no_grad()
+    def distance_field(self, img: np.ndarray) -> np.ndarray:
+        field = np.zeros_like(img)
+
+        for batch in range(len(img)):
+            fg_mask = img[batch] > 0.5
+
+            if fg_mask.any():
+                bg_mask = ~fg_mask
+
+                fg_dist = edt(fg_mask)
+                bg_dist = edt(bg_mask)
+
+                field[batch] = fg_dist + bg_dist
+
+        return field
+
+    def forward(
+        self, pred: torch.Tensor, target: torch.Tensor, debug=False
+    ) -> torch.Tensor:
+        """
+        Uses one binary channel: 1 - fg, 0 - bg
+        pred: (b, 1, x, y, z) or (b, 1, x, y)
+        target: (b, 1, x, y, z) or (b, 1, x, y)
+        """
+        assert pred.dim() == 4 or pred.dim() == 5, "Only 2D and 3D supported"
+        assert (
+            pred.dim() == target.dim()
+        ), "Prediction and target need to be of same dimension"
+
+        # pred = torch.sigmoid(pred)
+
+        # pred_dt = torch.from_numpy(self.distance_field(pred.cpu().numpy())).float()
+        # target_dt = torch.from_numpy(self.distance_field(target.cpu().numpy())).float()
+        pred_dt = torch.from_numpy(self.distance_field(pred.cpu().numpy())).float()
+        target_dt = torch.from_numpy(self.distance_field(target.cpu().numpy())).float()
+
+        pred_error = (pred - target) ** 2
+        distance = pred_dt ** self.alpha + target_dt ** self.alpha
+        distance = distance.cuda()
+
+        dt_field = pred_error * distance
+        loss = dt_field.mean()
+
+        if debug:
+            return (
+                loss.cpu().numpy(),
+                (
+                    dt_field.cpu().numpy()[0, 0],
+                    pred_error.cpu().numpy()[0, 0],
+                    distance.cpu().numpy()[0, 0],
+                    pred_dt.cpu().numpy()[0, 0],
+                    target_dt.cpu().numpy()[0, 0],
+                ),
+            )
+
+        else:
+            return loss
+
+class HausdorffERLoss(nn.Module):
+    """Binary Hausdorff loss based on morphological erosion"""
+
+    def __init__(self, alpha=2.0, erosions=10, **kwargs):
+        super(HausdorffERLoss, self).__init__()
+        self.alpha = alpha
+        self.erosions = erosions
+        self.prepare_kernels()
+
+    def prepare_kernels(self):
+        cross = np.array([cv.getStructuringElement(cv.MORPH_CROSS, (3, 3))])
+        bound = np.array([[[0, 0, 0], [0, 1, 0], [0, 0, 0]]])
+
+        self.kernel2D = cross * 0.2
+        self.kernel3D = np.array([bound, cross, bound]) * (1 / 7)
+
+    @torch.no_grad()
+    def perform_erosion(
+        self, pred: np.ndarray, target: np.ndarray, debug
+    ) -> np.ndarray:
+        bound = (pred - target) ** 2
+
+        if bound.ndim == 5:
+            kernel = self.kernel3D
+        elif bound.ndim == 4:
+            kernel = self.kernel2D
+        else:
+            raise ValueError(f"Dimension {bound.ndim} is nor supported.")
+
+        eroted = np.zeros_like(bound)
+        erosions = []
+
+        for batch in range(len(bound)):
+
+            # debug
+            erosions.append(np.copy(bound[batch][0]))
+
+            for k in range(self.erosions):
+
+                # compute convolution with kernel
+                dilation = convolve(bound[batch], kernel, mode="constant", cval=0.0)
+
+                # apply soft thresholding at 0.5 and normalize
+                erosion = dilation - 0.5
+                erosion[erosion < 0] = 0
+
+                if erosion.ptp() != 0:
+                    erosion = (erosion - erosion.min()) / erosion.ptp()
+
+                # save erosion and add to loss
+                bound[batch] = erosion
+                eroted[batch] += erosion * (k + 1) ** self.alpha
+
+                if debug:
+                    erosions.append(np.copy(erosion[0]))
+
+        # image visualization in debug mode
+        if debug:
+            return eroted, erosions
+        else:
+            return eroted
+
+    def forward(
+        self, pred: torch.Tensor, target: torch.Tensor, debug=False
+    ) -> torch.Tensor:
+        """
+        Uses one binary channel: 1 - fg, 0 - bg
+        pred: (b, 1, x, y, z) or (b, 1, x, y)
+        target: (b, 1, x, y, z) or (b, 1, x, y)
+        """
+        assert pred.dim() == 4 or pred.dim() == 5, "Only 2D and 3D supported"
+        assert (
+            pred.dim() == target.dim()
+        ), "Prediction and target need to be of same dimension"
+
+        # pred = torch.sigmoid(pred)
+
+        if debug:
+            eroted, erosions = self.perform_erosion(
+                pred.cpu().numpy(), target.cpu().numpy(), debug
+            )
+            return eroted.mean(), erosions
+
+        else:
+            eroted = torch.from_numpy(
+                self.perform_erosion(pred.cpu().numpy(), target.cpu().numpy(), debug)
+            ).float()
+
+            loss = eroted.mean()
+
+            return loss
+
+
+def tversky(y_true, y_pred):
+    smooth = 1
+    y_true_pos = torch.flatten(y_true)
+    y_pred_pos = torch.flatten(y_pred)
+    true_pos = torch.sum(y_true_pos * y_pred_pos)
+    false_neg = torch.sum(y_true_pos * (1-y_pred_pos))
+    false_pos = torch.sum((1-y_true_pos)*y_pred_pos)
+    alpha = 0.7
+    return (true_pos + smooth)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + smooth)
+
+def tversky_loss(y_true, y_pred):
+    return 1 - tversky(y_true,y_pred)
+
+def focal_tversky(y_true,y_pred):
+    pt_1 = tversky(y_true, y_pred)
+    gamma = 0.75
+    return torch.pow((1-pt_1), gamma)
+
+def softmax_helper(x):
+    # copy from: https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/utilities/nd_softmax.py
+    rpt = [1 for _ in range(len(x.size()))]
+    rpt[1] = x.size(1)
+    x_max = x.max(1, keepdim=True)[0].repeat(*rpt)
+    e_x = torch.exp(x - x_max)
+    return e_x / e_x.sum(1, keepdim=True).repeat(*rpt)
+
+class WeightedCrossEntropyLoss(torch.nn.CrossEntropyLoss):
+    """
+    Network has to have NO NONLINEARITY!
+    """
+    def __init__(self, weight=None):
+        super(WeightedCrossEntropyLoss, self).__init__()
+        self.weight = weight
+
+    def forward(self, inp, target):
+        target = target.long()
+        num_classes = inp.size()[1]
+
+        i0 = 1
+        i1 = 2
+
+        while i1 < len(inp.shape): # this is ugly but torch only allows to transpose two axes at once
+            inp = inp.transpose(i0, i1)
+            i0 += 1
+            i1 += 1
+
+        inp = inp.contiguous()
+        inp = inp.view(-1, num_classes)
+
+        target = target.view(-1,)
+        wce_loss = torch.nn.CrossEntropyLoss(weight=self.weight)
+
+        return wce_loss(inp, target)
+
+def sum_tensor(inp, axes, keepdim=False):
+    # copy from: https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/utilities/tensor_utilities.py
+    axes = np.unique(axes).astype(int)
+    if keepdim:
+        for ax in axes:
+            inp = inp.sum(int(ax), keepdim=True)
+    else:
+        for ax in sorted(axes, reverse=True):
+            inp = inp.sum(int(ax))
+    return inp
+
+
+def get_tp_fp_fn(net_output, gt, axes=None, mask=None, square=False):
+    """
+    net_output must be (b, c, x, y(, z)))
+    gt must be a label map (shape (b, 1, x, y(, z)) OR shape (b, x, y(, z))) or one hot encoding (b, c, x, y(, z))
+    if mask is provided it must have shape (b, 1, x, y(, z)))
+    :param net_output:
+    :param gt:
+    :param axes:
+    :param mask: mask must be 1 for valid pixels and 0 for invalid pixels
+    :param square: if True then fp, tp and fn will be squared before summation
+    :return:
+    """
+    if axes is None:
+        axes = tuple(range(2, len(net_output.size())))
+
+    shp_x = net_output.shape
+    shp_y = gt.shape
+
+    with torch.no_grad():
+        if len(shp_x) != len(shp_y):
+            gt = gt.view((shp_y[0], 1, *shp_y[1:]))
+
+        if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
+            # if this is the case then gt is probably already a one hot encoding
+            y_onehot = gt
+        else:
+            gt = gt.long()
+            y_onehot = torch.zeros(shp_x)
+            if net_output.device.type == "cuda":
+                y_onehot = y_onehot.cuda(net_output.device.index)
+            y_onehot.scatter_(1, gt, 1)
+
+    tp = net_output * y_onehot
+    fp = net_output * (1 - y_onehot)
+    fn = (1 - net_output) * y_onehot
+
+    if mask is not None:
+        tp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(tp, dim=1)), dim=1)
+        fp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fp, dim=1)), dim=1)
+        fn = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fn, dim=1)), dim=1)
+
+    if square:
+        tp = tp ** 2
+        fp = fp ** 2
+        fn = fn ** 2
+
+    tp = sum_tensor(tp, axes, keepdim=False)
+    fp = sum_tensor(fp, axes, keepdim=False)
+    fn = sum_tensor(fn, axes, keepdim=False)
+
+    return tp, fp, fn
+
+
+class SoftDiceLoss(nn.Module):
+    def __init__(self, apply_nonlin=None, batch_dice=False, do_bg=True, smooth=1.,
+                 square=False):
+        """
+        paper: https://arxiv.org/pdf/1606.04797.pdf
+        """
+        super(SoftDiceLoss, self).__init__()
+
+        self.square = square
+        self.do_bg = do_bg
+        self.batch_dice = batch_dice
+        self.apply_nonlin = apply_nonlin
+        self.smooth = smooth
+
+    def forward(self, x, y, loss_mask=None):
+        shp_x = x.shape
+
+        if self.batch_dice:
+            axes = [0] + list(range(2, len(shp_x)))
+        else:
+            axes = list(range(2, len(shp_x)))
+
+        if self.apply_nonlin is not None:
+            x = self.apply_nonlin(x)
+
+        tp, fp, fn = get_tp_fp_fn(x, y, axes, loss_mask, self.square)
+
+        dc = (2 * tp + self.smooth) / (2 * tp + fp + fn + self.smooth)
+
+        if not self.do_bg:
+            if self.batch_dice:
+                dc = dc[1:]
+            else:
+                dc = dc[:, 1:]
+        dc = dc.mean()
+
+        return -dc
+
+
+class ExpLog_loss(nn.Module):
+    """
+    paper: 3D Segmentation with Exponential Logarithmic Loss for Highly Unbalanced Object Sizes
+    https://arxiv.org/pdf/1809.00076.pdf
+    """
+    def __init__(self, gamma=0.3):
+        super(ExpLog_loss, self).__init__()
+        self.wce = WeightedCrossEntropyLoss()
+        self.dc = SoftDiceLoss(apply_nonlin=softmax_helper)
+        self.gamma = gamma
+
+    def forward(self, net_output, target):
+        dc_loss = -self.dc(net_output, target) # weight=0.8
+        wce_loss = self.wce(net_output, target) # weight=0.2
+        # with torch.no_grad():
+        #     print('dc loss:', dc_loss.cpu().numpy(), 'ce loss:', ce_loss.cpu().numpy())
+        #     a = torch.pow(-torch.log(torch.clamp(dc_loss, 1e-6)), self.gamma)
+        #     b = torch.pow(-torch.log(torch.clamp(ce_loss, 1e-6)), self.gamma)
+        #     print('ExpLog dc loss:', a.cpu().numpy(), 'ExpLogce loss:', b.cpu().numpy())
+        #     print('*'*20)
+        explog_loss = 0.8*torch.pow(-torch.log(torch.clamp(dc_loss, 1e-6)), self.gamma) + \
+            0.2*wce_loss
+
+        return explog_loss
